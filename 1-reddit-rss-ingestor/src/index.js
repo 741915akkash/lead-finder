6246a95cd5b8145e1292fde789eb1a5e418ec741
Akash Saini <cheetah74191915@gmail.com>
2026-06-22 23:44:03 +0530

    removed clean up

--- 1-reddit-rss-ingestor/src/index.js
+++ 1-reddit-rss-ingestor/src/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const subreddits = require('./config/subreddits');
 
 const { syncSubreddit } = require('./services/sync-subreddit');
-const { cleanupLowScorePosts } = require('./services/cleanup-low-score-posts');
 
 async function run() {
   for (const subreddit of subreddits) {
@@ -12,12 +11,6 @@ async function run() {
     } catch (err) {
       console.error(subreddit, err.message);
     }
-  }
-
-  try {
-    await cleanupLowScorePosts();
-  } catch (err) {
-    console.error('cleanup failed:', err.message);
   }
 
   process.exit(0);
