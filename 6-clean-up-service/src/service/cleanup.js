169e6f88bb4851e45abedc6474dfd7c1796d8da1
Akash Saini <cheetah74191915@gmail.com>
2026-06-28 04:02:07 +0530

    cleans post score equal 0

--- 6-clean-up-service/src/service/cleanup.js
+++ 6-clean-up-service/src/service/cleanup.js
@@ -1,13 +1,13 @@
 const { supabase } = require('../db/db');
 
 async function cleanupLowScorePosts() {
-  const { error } = await supabase.from('reddit_posts').delete().lt('score', 60).not('score', 'is', null);
+  const { error } = await supabase.from('reddit_posts').delete().eq('score', 0).not('score', 'is', null);
 
   if (error) {
     throw error;
   }
 
-  console.log('Deleted analyzed posts with score < 60');
+  console.log('Deleted analyzed posts with score = 0');
 }
 
 module.exports = {
